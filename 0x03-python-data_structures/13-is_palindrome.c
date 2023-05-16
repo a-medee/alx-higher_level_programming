acaa0aae2960a603e4e0158c5ab0064b508aafa7
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-05-16 09:52:38 +0100

    made initial corrections

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -36,6 +36,7 @@ int is_palindrome(listint_t **head)
 			}
 			else
 				break;
+
 		}
 		if (_size == size_cp / 2)
 			return (1);
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,11 +4,12 @@ def print_reversed_list_integer(my_list=[]):
     """Prints all integers of a list, in reverse order
 
     Args:
-        my_list: a list element whose value is to be printed
+        my_list: a list element whose values are to be printed
 
     Return:
         None
     """
-    list_size = len(my_list) - 1
-    for i in range(list_size, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list:
+        list_size = len(my_list) - 1
+        for i in range(list_size, -1, -1):
+            print("{:d}".format(my_list[i]))
