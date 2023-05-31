812caf41ae74d8b06a6714d03a03b3f3c0d8e60d
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-01 00:03:20 +0100

    repected pycodestyle

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    result = 0
+    """This function does a task
 
+    Args:
+        a: The first argument.
+        b: The second argument.
+
+    Returns:
+        The result of the calculation.
+  """
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception("Too far")
             else:
                 result = result + (a ** b / i)
-        except:
+        except Exception:
             result = a + b
             break
         finally:
             pass
-
     return result
