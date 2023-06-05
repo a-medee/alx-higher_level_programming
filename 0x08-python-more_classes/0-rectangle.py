16f9144ecbf2d799d7a342b9f11f5fb7a869d21a
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-05 22:15:21 +0100

    First commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module devises an user-defined rectangle class
+"""
+class Rectangle:
+    """This is an user-defined rectangle class
+
+    Attributes:
+        None
+    """
+    pass
