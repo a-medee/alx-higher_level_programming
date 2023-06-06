3b0dd4e3867f04fa3b9934d9e925b956b110e65e
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-06 02:52:30 +0100

    dropped the use of getattr, replaced by self.print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -111,14 +111,14 @@ class Rectangle:
     def __str__(self):
         """Return a formal nicely representation of a rectangle object
         """
-        a = getattr(self, "print_symbol")
+        a = ""
 
         if self.width == 0 or self.height == 0:
             return a
 
         for i in range(self.height):
             for j in range(self.width):
-                a += str(a)
+                a += str(self.print_symbol)
             if i != self.height - 1:
                 a += "\n"
         return a
