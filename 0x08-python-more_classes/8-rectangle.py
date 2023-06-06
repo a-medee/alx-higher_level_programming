31e1914eab548e2d623b2c20d2c2cb16ba56ddad
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-06 14:45:06 +0100

    added class method on the 10th task's file

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -143,3 +143,7 @@ class Rectangle:
             return rect_2
         if rect_2.area() == rect_1.area():
             return rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
