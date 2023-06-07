39106006db165b57377a75bedd7d65ba35ba46b9
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-07 04:04:10 +0100

    added docstrings to the class and to the module

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""This moduled devises a way to prevent dynamic allocation
+"""
 class LockedClass(object):
+    """This class is built with the idea to prevent the user
+    from dynamicly creating attributes to the class
+    """
     __slots__ = ['first_name']
