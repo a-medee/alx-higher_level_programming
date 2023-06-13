740cf4834e9f9436d92219a9102407891011a8a4
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-13 15:23:08 +0100

    added the last adanvanced task

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This modules devises a function called add_attribute
+"""
+
+def add_attribute(obj , att_name, att_value):
+    """A function that adds a new attribute to an object
+    if it’s possible:
+
+    Args:
+        obj: a class object
+        att_name: the attribute name to be added
+        att_value: the attribute value
+
+    Returns:
+        None
+    """
+    if not issubclass(type(obj), (str, int, float, tuple, dict, list, set)):
+        if obj.__dict__ == {} or obj.__dict__:
+            obj.__dict__[att_name]= att_value
+    else:
+        raise TypeError("can't add new attribute")
