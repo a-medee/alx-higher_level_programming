5694e93da54ed6ca55ff89f0b44ae68e2252ba18
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-13 01:25:38 +0100

    added th3 5th file

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,5 +16,7 @@ def inherits_from(obj, a_class):
     Returns:
         Bool object
     """
-
-    pass
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return (True)
+    else:
+        return (False)
