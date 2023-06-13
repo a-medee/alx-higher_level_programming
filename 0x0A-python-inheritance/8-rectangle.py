5ba4e136b4f096cb796a5a30f4f84863872a3b33
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-13 13:05:29 +0100

    changed module import format

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This module defines a non-empty class named Rectangle
 """
-import sys
-
+sys = __import__("sys")
 sys.path.insert(1, "./7-base_geometry")
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
@@ -10,9 +9,6 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """A classe that inherits from the BaseGeometry class defined
     earlier
-
-    Args:
-        None
 
     Attributes:
         None
@@ -27,6 +23,10 @@ class Rectangle(BaseGeometry):
 
         Returns:
             None
+
+        Raises:
+            TypeError: if the value is not an integer
+            ValueError: if the value is zero or negative
         """
         super().__init__()
 
