c984c590df8a72036ccdab3c03b9e365707d7661
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-06-14 17:08:18 +0100

    added the first advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" This modules devises a function named append_after """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function inserts a line of text to a file, after each
+    line containing a specific string
+
+    Args:
+        filename: a path to the file name
+        search_string: the string to be searched
+        new_string: the string to insert after search_string
+
+    Returns:
+        None
+    """
+
+    my_file_list = list()
+
+    with open(filename, "r+", encoding="utf-8") as a_file:
+        for line in a_file:
+            my_file_list.append(line)
+
+    for line in my_file_list:
+        if search_string in line:
+            my_file_list.insert(my_file_list.index(line) + 1, new_string)
+
+    with open(filename, "w", encoding="utf-8") as a_file:
+        a_file.write("".join(my_file_list))
