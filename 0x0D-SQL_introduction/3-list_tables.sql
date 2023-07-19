bd523c255c0277cb8f6534b46281dffca0927d6c
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-07-19 01:37:34 +0100

    Fourth commit

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,3 @@
 -- a script that lists all the tables of a database in your MySQL server.
 
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = database_name;
+SHOW TABLES;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- a script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server.
 
-create_table_statement = SHOW CREATE TABLE table_name;
-
-SELECT create_table_statement;
+SHOW CREATE TABLE `first_table`;
