ede3eb151f031126db6413938d8e278ffb0eb999
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-07-19 21:27:56 +0100

    made corrections

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,8 +4,7 @@ SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = (
-        SELECT tv_genres.id
-  	FROM tv_genres
-  	WHERE tv_genres.name = 'Comedy'
-) ORDER BY tv_shows.title;
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
