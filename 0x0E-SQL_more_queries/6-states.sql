029481efbcade395bcaf098ca78b46ac78712ce1
Amédée Megnigbeto <amedee.megni.gbeto@protonmail.ch>
2023-07-19 21:19:14 +0100

    made corrections on the 7th file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,10 @@
 --  a script that creates the database hbtn_0d_usa and the table states on
 -- MySQL server.
 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_us.states (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+  id INT UNIQUE NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id)
 );
